1dde430e8fc77d8fad3217a77a34a8b23c15919b
ashwinkesireddy <95117037+ashwinkesireddy@users.noreply.github.com>
2023-06-25 10:41:49 -0700

    Add files via upload

---
+++ P3L3Firewall.py
@@ -0,0 +1,75 @@
+from pox.core import core
+import pox.openflow.libopenflow_01 as of
+from pox.lib.revent import *
+from pox.lib.util import dpidToStr
+from pox.lib.addresses import EthAddr,IP_ANY
+from collections import namedtuple
+import os
+''' New imports here ... '''
+from pox.lib.packet.ethernet import ethernet, ETHER_BROADCAST,ETHER_ANY
+from pox.lib.addresses import IPAddr
+import pox.lib.packet as pkt
+from pox.lib.packet.arp import arp
+from pox.lib.packet.ipv4 import ipv4
+from pox.lib.packet.icmp import icmp
+
+import time
+log = core.getLogger()
+priority = 50000
+
+class Firewall (EventMixin):
+
+        def __init__ (self):
+		self.listenTo(core.openflow)
+		self.fwconfig = list()
+                self.ptbl = {}
+
+	def installFlow(self, event, offset, srcmac, dstmac, srcip, dstip, sport, dport, nwproto):
+		msg = of.ofp_flow_mod()
+		match = of.ofp_match()
+		if(srcip != None):
+			match.nw_src = IPAddr(srcip)
+		if(dstip != None):
+			match.nw_dst = IPAddr(dstip)	
+		match.nw_proto = int(nwproto)
+		match.dl_src = srcmac
+		match.dl_dst = dstmac
+		match.tp_src = sport
+		match.tp_dst = dport
+		match.dl_type = pkt.ethernet.IP_TYPE
+		msg.match = match
+		msg.hard_timeout = of.OFP_FLOW_PERMANENT
+		msg.idle_timeout = of.OFP_FLOW_PERMANENT #200
+		msg.priority = priority + offset		
+		event.connection.send(msg)
+
+	def _handle_PacketIn(self, event):
+		packet = event.parsed
+		match = of.ofp_match.from_packet(packet)
+
+		if(match.dl_type == packet.IP_TYPE):
+		  ip_packet = packet.payload
+		  print "Ip_packet.protocol = ", ip_packet.protocol
+		  if ip_packet.protocol == ip_packet.TCP_PROTOCOL:
+			log.debug("TCP it is !")
+                        tcppkt = ip_packet.payload
+                        print tcppkt.SYN,tcppkt.flags
+                        if tcppkt.SYN:
+                            if not match.dl_src in self.ptbl:
+                                self.ptbl[match.dl_src]=[match.nw_src]
+                            else:
+                                self.ptbl[match.dl_src].append(match.nw_src)
+                            print self.ptbl
+                            if len(self.ptbl[match.dl_src])>5:
+                                #print "not issuing any rule"
+                                self.installFlow(event, 1, 
+                                        match.dl_src, match.dl_dst, 
+                                        None, match.nw_dst, 
+                                        None, None, match.nw_proto)
+                                self.ptbl={}
+   
+def launch ():
+	'''
+	Starting the Firewall module
+	'''
+	core.registerNew(Firewall)
